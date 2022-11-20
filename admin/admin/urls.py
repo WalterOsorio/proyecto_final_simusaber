8bbf90f196861e009b3230d9d370579b18a4808c
WalterOsorio <weosorio60@misena.edu.co>
2022-11-20 17:50:46 -0500

    segundo commit

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('profiles/', include(('apps.profiles.urls', 'profiles'), namespace="profiles")),
+    # path('locations/', include(('apps.locations.urls', 'locations'), namespace="locations")),
 ]
--- admin/apps/profiles/urls.py
+++ admin/apps/profiles/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
 	path('home/',home_view, name='home'),
-	# path('apiSendCodeSMS',send_code_sms, name='apiSendCodeSMS'),
-	# path('apiValidateCodeSMS',validate_code_sms, name='apiValidateCodeSMS')
 
 ]
